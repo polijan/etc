9a9d377ade29105143f9563ddab7428ba415334f
notevenodd <notevenodd@users.noreply.github.com>
2020-04-20 18:18:30 +0300

    cp+, alias for cp so that when only one arg is given, it prompts for dst (and propose a timestamped backup as initial choice)

--- true/.config/sh-includes/general.sh
+++ true/.config/sh-includes/general.sh
@@ -3,10 +3,14 @@
 alias ls='ls+'
 alias cal='cal+'
 
-alias cp='cp -i'
 alias rm='rm -i'
 
 if command -v mv+ >/dev/null
    then alias mv='mv+'
    else alias mv='mv -i'
+fi
+
+if command -v cp+ >/dev/null
+   then alias cp='cp+'
+   else alias cp='cp -i'
 fi
