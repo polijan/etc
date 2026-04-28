129abf74ba18d16ca7e4144c6d5ce819a9a1fb5f
polijan <6453311+polijan@users.noreply.github.com>
2026-04-29 00:27:33 +0300

    general stuff to include

--- true/.config/sh-includes/general.sh
+++ true/.config/sh-includes/general.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+alias l='ls+'
 alias ls='ls+'
 alias cal='cal+'
 alias diff='diff+'
@@ -23,3 +24,8 @@ alias grep='grep --color'
 # ALWAYS ONLY DO SYMLINKS (use \ln to override)
 alias ln='ln -s'
 # add too: -i option from coreutils ???
+
+# man: by setting this variable, man format it ouput with a given line width.
+# (perhaps we should avoid hardcoding it and make a `man` wrapper that sets
+#  MANWDITH only when terminal size is larger than that.)
+export MANWIDTH=74
